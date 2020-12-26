527b9b788c7ad3304820b5f0e8e46285f9195854
Quan Bao <quy123bao20@gmail.com>
2020-12-26 14:09:22 +0700

    blah

--- middleware/verifyRole.js
+++ middleware/verifyRole.js
@@ -4,6 +4,9 @@ module.exports.verifyRole = (...roles) => {
   return (req, res, next) => {
     const authHeader = req.headers["authorization"];
     let token = authHeader && authHeader.split(" ")[1];
+    if (token === "undefined") {
+      token = null;
+    };
     if (!token || process.env.NODE_ENV === "production")
       token = req.signedCookies.idBloggerUser;
     if (!token) return res.status(401).send({ error: "Access denied" });
