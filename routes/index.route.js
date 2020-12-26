532f1ba651be121ed149cd0e8bfe224ab968ff61
Quan Bao <quy123bao20@gmail.com>
2020-12-26 11:45:56 +0700

    adding router

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const path = require("path");
-router.get("/tags/:tagId", (req, res) => {
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
+router.get("/account/edit", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
+router.get("/posts/search/:title", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
@@ -8,7 +16,31 @@ router.get("/posts/user/:userId", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
+router.get("/auth/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
+router.get("/auth/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
+router.get("/tags/:tagId", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
+router.get("/tags", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
+router.get("/blog/:postId/:isPending", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
 router.get("/blog/:postId", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+
+router.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
